3442c88e27663ce120e6a6fd87da48477e3fa1c0
Rinor Rama <rinor.rama95@gmail.com>
2021-11-18 15:01:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let message = `These are the users:`;
+for (const user of window.users) {
+	message += `\nUserID: ${user.id}, UserName: ${user.name}, From: ${user.address.city}`;
+}
+alert(message);
+
+let selectedUserID = Number(prompt("Enter your UserID:"));
+while (selectedUserID < 1 || selectedUserID > 10) {
+	selectedUserID = Number(prompt("This user doesn't exist, please type a number between 1 and 10"));
+}
+let selectedUser = window.users.find((user) => user.id === selectedUserID);
+let selectedUserToDoList = window.todos.filter((todo) => todo.userId === selectedUserID);
+
+message = `These are ${selectedUser.name}'s tasks: `;
+for (const todo of selectedUserToDoList) {
+	message += `\nTitle: ${todo.title}`;
+}
+
+alert(message);
